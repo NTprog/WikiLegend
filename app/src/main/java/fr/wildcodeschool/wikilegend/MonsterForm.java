4072589aaa01e8b053099d96f9271c377d979272
Thibault Panis <thib.panis@gmail.com>
2018-09-13 09:54:38 +0200

    Adding drawable and advancing in setting of the monsterform layout

--- app/src/main/java/fr/wildcodeschool/wikilegend/MonsterForm.java
+++ app/src/main/java/fr/wildcodeschool/wikilegend/MonsterForm.java
@@ -1,7 +1,12 @@
 package fr.wildcodeschool.wikilegend;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MonsterForm extends AppCompatActivity {
 
@@ -9,9 +14,6 @@ public class MonsterForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monster_form);
-
-
-
 
 
 
