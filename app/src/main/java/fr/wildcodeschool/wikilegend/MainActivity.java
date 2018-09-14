ba41d1da7b4a23bdf8febf4a0abf5ce592dbfff4
Tom Noden <ntprogramation@gmail.com>
2018-09-14 13:36:49 +0200

    implementation du bouton Mainactivity vers MonsterForm pour fire lion

--- app/src/main/java/fr/wildcodeschool/wikilegend/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/wikilegend/MainActivity.java
@@ -1,13 +1,34 @@
 package fr.wildcodeschool.wikilegend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button fireButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fireButton = (Button) findViewById(R.id.fireButton);
+        fireButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMonsterForm();
+            }
+
+        });
     }
+
+    public void openMonsterForm(){
+        Intent intent = new Intent(this, MonsterForm.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
 }
