8a3cb3e12739dfbf623c01d6aa2c8e57a9f96d8c
Alain Somet <sometalain@yahoo.fr>
2018-09-14 11:31:33 +0200

    new commit menu monstre

--- app/src/main/java/fr/wildcodeschool/wikilegend/MonsterForm.java
+++ app/src/main/java/fr/wildcodeschool/wikilegend/MonsterForm.java
@@ -6,11 +6,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.widget.Button;
-=======
 import android.view.View;
->>>>>>> 5bcee89b361e4b6410c3a328f162c73d217c7bda
+
 
 public class MonsterForm extends AppCompatActivity {
 
@@ -19,15 +17,6 @@ public class MonsterForm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monster_form);
 
-
-
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 5bcee89b361e4b6410c3a328f162c73d217c7bda
     }
 
 
